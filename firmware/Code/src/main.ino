910ae6be8bb8db873d2e3e21ed5fe605fc5065c3
Moabe Barbosa Alves <moabebarbosaa@gmail.com>
2022-03-20 00:52:37 -0300

    ajuste

--- firmware/Code/src/main.ino
+++ firmware/Code/src/main.ino
@@ -1,29 +1,28 @@
-#include "DHT.h"
-#include "AdafruitIO_WiFi.h"
-#include "ReadSensor.h"
+#include <read_sensor.h>
+
 /************************** Config. comunicacion ***********************************/
 
 #define IO_USERNAME "mbcode"
-#define IO_KEY "aio_jlZi50njmFQxlDaAFHExPLVYRxVu"
+#define IO_KEY "aio_EZbN39haHRd1aJSNfNNRd2FdnN1M"
 
 /******************************* WIFI **************************************/
 
 #define WIFI_SSID "POWERNET_INTELBRAS"
 #define WIFI_PASS "catsix623"
 
+#include "AdafruitIO_WiFi.h"
+
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
-/************************ Example Starts Here *******************************/
-
-#define DHTPIN D1
-#define DHTTYPE DHT11 
-DHT dht(DHTPIN, DHTTYPE);
+#define SENSORSOLO A0
 
 AdafruitIO_Feed *temperatura = io.feed("temperatura");
-AdafruitIO_Feed *humedad = io.feed("humedad");
+AdafruitIO_Feed *umidade = io.feed("umidade");
+AdafruitIO_Feed *umidadeSolo = io.feed("umidade do solo");
+
+rd_sr rd(SENSORSOLO);
 
 void setup() {
-  
   Serial.begin(115200);
   while(! Serial);
   Serial.print("Conectando a Adafruit IO");
@@ -34,22 +33,21 @@ void setup() {
   }
   Serial.println();
   Serial.println(io.statusText());
-  
-  dht.begin();
 }
 
 void loop() {
   delay(5000);
   io.run();
-
-
-  float h = ReadSensor.readHumidity();
-  float t = ReadSensor.readTemperature();
-  // Serial.print("temperatura: ");
-  // Serial.print(t);
-  // Serial.print(" humedad: ");
-  // Serial.println(h);
+  float h = rd.readHumidityDHT();
+  float t = rd.readTemperatureDHT();
+  int u = ( rd.readUmidadeSolo() / 1024) * 100;
+  Serial.println("Solo: ");
+  Serial.println(u);
+  Serial.print("temperatura: ");
+  Serial.print(t);
+  Serial.print(" humedad: ");
+  Serial.println(h);
   temperatura->save(t);
-  humedad->save(h); 
- 
+  umidade->save(h);
+  umidadeSolo->save(u);
 }
